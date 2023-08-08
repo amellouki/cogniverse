6d2f2a1f6b07be5d6595e92f664d2d2dc181dbe7
AMellouki <abderrahim.mellouki.p@gmail.com>
2023-08-08 20:51:05 +0100

    temp: hard-code allowed origins

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -8,7 +8,7 @@ dotenv.config({ path: './.env.local' });
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.ALLOWED_DOMAINS?.split(','), // Allow these origins
+    origin: ['https://www.cogniverse.ai'], // Allow these origins
     methods: ['GET', 'HEAD', 'PUT', 'POST', 'DELETE'], // Allow these HTTP methods
     allowedHeaders: ['Content-Type', 'Accept', 'Authorization'], // Allow these headers
     credentials: true, // Allow cookies and credentials to be sent with requests
