18e4323bd5b9bf6eb2ab1725bfd6194f343fe2f4
Abderrahim Mellouki <abderrahim.mellouki.p@gmail.com>
2024-01-04 03:28:47 +0100

    implment file selection & previews

--- packages/frontend/src/pages/data/index.tsx
+++ packages/frontend/src/pages/data/index.tsx
@@ -1,17 +1,6 @@
 import { NextPageWithLayout } from '@/pages/_app';
 import { getLayout } from '@/components/Layouts/DefaultLayout/BotNestedLayout';
 import Button from '@/components/Button';
-import apiInstance from '@/helpers/api';
-import { useRouter } from 'next/router';
-import {
-  createContext,
-  Dispatch,
-  SetStateAction,
-  useContext,
-  useState,
-} from 'react';
-import { useInfiniteQuery, useQuery } from 'react-query';
-import { noop } from 'lodash';
 import Link from 'next/link';
 
 const Page: NextPageWithLayout = () => {
